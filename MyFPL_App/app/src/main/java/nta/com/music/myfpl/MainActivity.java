09c6471620801459c65119bb9273b7c390ff9794
josephanh <josephanh129@gmail.com>
2023-07-20 15:17:45 +0700

    feat/bottom navigation

--- MyFPL_App/app/src/main/java/nta/com/music/myfpl/MainActivity.java
+++ MyFPL_App/app/src/main/java/nta/com/music/myfpl/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ChipNavigationBar menu = findViewById(R.id.menu);
+        menu.setItemSelected(R.id.bt_home, true);
     }
 }
