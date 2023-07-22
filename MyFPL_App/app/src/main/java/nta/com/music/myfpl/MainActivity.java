0a8d3646e433fec74784662f93ff21b76f2a05c9
josephanh <josephanh129@gmail.com>
2023-07-22 07:37:01 +0700

    feat/new handler and executor

--- MyFPL_App/app/src/main/java/nta/com/music/myfpl/MainActivity.java
+++ MyFPL_App/app/src/main/java/nta/com/music/myfpl/MainActivity.java
@@ -1,5 +1,6 @@
 package nta.com.music.myfpl;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.view.Window;
@@ -18,19 +22,47 @@ import android.widget.FrameLayout;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import nta.com.music.myfpl.fragments.HomeFragment;
 import nta.com.music.myfpl.fragments.ScheduleFragment;
 import nta.com.music.myfpl.fragments.UserFragment;
 
 
 public class MainActivity extends AppCompatActivity {
-    FrameLayout frameLayout;
+
+    ThreadPoolExecutor executor;
+    private Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Fragment fragment = new HomeFragment();
+            int idSelected = msg.arg1;
+            if(idSelected == R.id.bt_home) {
+                fragment = new HomeFragment();
+            }
+            if(idSelected == R.id.bt_schedule) {
+                fragment = new ScheduleFragment();
+            }
+            if(idSelected == R.id.bt_user) {
+                fragment = new UserFragment();
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.pop_enter,R.animator.fade_out)
+                    .replace(R.id.layout_fragment, fragment)
+                    .commit();
+        }
+    };
+
+
     ChipNavigationBar bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
         setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -47,35 +79,21 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("NonConstantResourceId")
             @Override
             public void onItemSelected(int i) {
-//                setMenuNavigation();
+                setMenuNavigation();
             }
         });
     }
 
 
     public void setMenuNavigation(){
-        new Thread(new Runnable() {
+        executor.execute(new Runnable() {
             @Override
             public void run() {
-
-                Fragment fragment = new HomeFragment();
-
-                if(bottomNavigation.getSelectedItemId() == R.id.bt_home) {
-                    fragment = new HomeFragment();
-                }
-                if(bottomNavigation.getSelectedItemId() == R.id.bt_schedule) {
-                    fragment = new ScheduleFragment();
-                }
-                if(bottomNavigation.getSelectedItemId() == R.id.bt_user) {
-                    fragment = new UserFragment();
-                }
-                getSupportFragmentManager().beginTransaction()
-                        .setCustomAnimations(R.anim.pop_enter,R.animator.fade_out)
-                        .replace(R.id.layout_fragment, fragment)
-                        .commit();
-
+                Message message = new Message();
+                message.arg1 = bottomNavigation.getSelectedItemId();
+                handler.sendMessage(message);
             }
-        }).start();
+        });
     }
 
 
