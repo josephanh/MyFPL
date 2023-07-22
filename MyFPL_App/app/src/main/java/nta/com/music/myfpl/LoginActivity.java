066c5ec7bb68e3a0f4e7eb2893a6e4d97b9f8fc9
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2023-07-22 12:32:51 +0700

    feat/login_google

--- MyFPL_App/app/src/main/java/nta/com/music/myfpl/LoginActivity.java
+++ MyFPL_App/app/src/main/java/nta/com/music/myfpl/LoginActivity.java
@@ -1,23 +1,34 @@
 package nta.com.music.myfpl;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.muratozturk.click_shrink_effect.ClickShrinkEffect;
-
-import java.lang.reflect.Parameter;
 
 import render.animations.Bounce;
 import render.animations.Render;
@@ -27,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
 
     LinearLayout login_linear;
     ImageView logoFPT; LinearLayout choice_campus, login_google;
+    GoogleSignInClient gsc;
 
     @SuppressLint("ResourceAsColor")
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -53,6 +65,26 @@ public class LoginActivity extends AppCompatActivity {
         choice_campus = findViewById(R.id.choiceCampus);
         login_google = findViewById(R.id.loginGoogle);
 
+        // yêu cầu người dùng cung cấp gmail và request gmail luôn
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSignInClient.signOut();
+        gsc = GoogleSignIn.getClient(LoginActivity.this, gso);
+
+        login_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent googleIntent = gsc.getSignInIntent();
+                googleLauncher.launch(googleIntent);
+
+            }
+        });
+
+
+
 //      animation
         Render render = new Render(getApplicationContext());
         //      Set Animation
@@ -66,6 +98,30 @@ public class LoginActivity extends AppCompatActivity {
         new ClickShrinkEffect(choice_campus, 0.98f, 50);
         new ClickShrinkEffect(login_google, 0.98f, 50);
     }
+
+    // result google
+    ActivityResultLauncher<Intent> googleLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+                    try {
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        String email = account.getEmail();
+                        String name = account.getDisplayName();
+                        String image = String.valueOf(account.getPhotoUrl());
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } catch (Exception e) {
+                        Log.d(">>>>>>>>>>>>TAG", "onActivityResult Erron: " + e.getMessage());
+                    }
+                }
+            });
+    // end result google
 
     public static void setWindowFlag(Activity activity, final int bits, boolean on) {
         Window win = activity.getWindow();
