14c619d68c9bd6b59e5664c5cc58a4066dade549
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2023-07-28 12:34:19 +0700

    feat/layout_event

---
+++ MyFPL_App/app/src/main/java/nta/com/music/myfpl/model/Event.java
@@ -0,0 +1,27 @@
+package nta.com.music.myfpl.model;
+
+public class Event {
+    private int resourceId;
+    private String name;
+
+    public Event(int resourceId, String name) {
+        this.resourceId = resourceId;
+        this.name = name;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
